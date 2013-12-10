79e9ad51c0f9551b2d23f6b0150cef6e9cd14ba4
xjstratedgebx <jarret@solvomedia.com>
2013-12-09 20:59:34 -0500

    Adding in the index file and tweaking it so that it would work if Halligan Core was used as just a simple, all-contained library.

---
+++ Public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Begin benchmark timer
+ */
+define('HALLIGAN_START', microtime(TRUE));
+
+
+/**
+ * Include the paths configuration before we change working directories
+ */
+require realpath(__DIR__ . '/../Config/Paths.php');
+
+
+/**
+ * Change the current working directory
+ */
+chdir(realpath(__DIR__ . '/..'));
+
+
+/**
+ * Load the Halligan bootstrap
+ */
+require 'Halligan.php';
+
+
+/* End of file index.php */
