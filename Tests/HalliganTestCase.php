2602623e45ad5f6d3f2511861fa0f7330d1758cd
xjstratedgebx <jarret@solvomedia.com>
2014-05-03 21:27:45 -0400

    Updates to the HalliganTestCase class in order to make input testing a whole lot easier. Might really only be minimally easier, but at least the input list can be made consistent.

--- Tests/HalliganTestCase.php
+++ Tests/HalliganTestCase.php
@@ -2,10 +2,198 @@
 
 class HalliganTestCase extends PHPUnit_Framework_TestCase {
 
+	protected $inputs = array();
+	protected $input_modifiers = array();
+
+	const INPUT_CLASS_BOOL			=	"InputClassBool";
+	const INPUT_CLASS_INT			=	"InputClassInt";
+	const INPUT_CLASS_INT_VAL		=	"InputClassIntVal";
+	const INPUT_CLASS_INT_GTZ		=	"InputClassIntGtz";
+	const INPUT_CLASS_INT_VAL_GTZ	=	"InputClassIntValGtz";
+	const INPUT_CLASS_ARRAY			=	"InputClassArray";
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
 	protected function setUp()
 	{
 		PDOStatement_Stub::reset();
 		Factory::reset();
+		$this->input_modifiers = array();
+		$this->inputs = array(
+			TRUE,
+			FALSE,
+			NULL,
+			-1,
+			"-1",
+			-1.1,
+			"-1.1",
+			"-1,000",
+			"-1,000.1",
+			0,
+			"0",
+			0.0,
+			"0.0",
+			1,
+			"1",
+			1.1,
+			"1.1",
+			"1,000",
+			"1,000.1",
+			"A",
+			"AA",
+			"",
+			" ",
+			"\r",
+			"\n",
+			array(),
+			array(1),
+			array("1"),
+			array("test" => "value"),
+			(object) array(),
+			(object) array("test" => "value")
+		);
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function getInputs()
+	{
+		return $this->inputs;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function subInput(/* polymorphic */)
+	{
+		foreach(func_get_args() as $input)
+		{
+			$this->input_modifiers[] = (object) array(
+				"type" => "sub",
+				"value" => $input
+			);
+		}
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function subInputClass($class)
+	{
+		switch($class)
+		{
+			case self::INPUT_CLASS_BOOL:
+				$this->subInput(TRUE);
+				$this->subInput(FALSE);
+				break;
+
+			case self::INPUT_CLASS_INT:
+				$this->subInput(-1, 0, 1);
+				break;
+
+			case self::INPUT_CLASS_INT_GTZ:
+				$this->subInput(1);
+				break;
+
+			case self::INPUT_CLASS_INT_VAL:
+				$this->subInput(-1, "-1", 0, "0", 1, "1");
+				break;
+
+			case self::INPUT_CLASS_INT_VAL_GTZ:
+				$this->subInput(1, "1");
+				break;
+
+			case self::INPUT_CLASS_ARRAY:
+				$this->subInput(array(), array(1), array("1"), array("test" => "value"));
+				break;
+		}
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function subInputBool()
+	{
+		$this->subInputClass(self::INPUT_CLASS_BOOL);
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function subInputInt($greater_than_zero = FALSE)
+	{
+		if($greater_than_zero)
+		{
+			$this->subInputClass(self::INPUT_CLASS_INT_GTZ);
+		}
+		else
+		{
+			$this->subInputClass(self::INPUT_CLASS_INT);		
+		}
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function subInputIntVal($greater_than_zero = FALSE)
+	{
+		if($greater_than_zero)
+		{
+			$this->subInputClass(self::INPUT_CLASS_INT_VAL_GTZ);
+		}
+		else
+		{
+			$this->subInputClass(self::INPUT_CLASS_INT_VAL);		
+		}
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function subInputArray()
+	{
+		$this->subInputClass(self::INPUT_CLASS_ARRAY);
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+
+	public function buildInputs()
+	{
+		$tmp = $this->getInputs();
+
+		foreach($this->input_modifiers as $mod)
+		{
+			switch($mod->type)
+			{
+				case "sub":
+					$this->removeInputValue($tmp, $mod->value);
+					break;
+			}
+		}
+
+		return $tmp;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public function removeInputValue(&$tmp, $value)
+	{
+		$tmp = array_filter($tmp, function($val) use ($value) { return $val !== $value; });
 	}
 
 }
