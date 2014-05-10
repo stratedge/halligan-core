3b00c9ceea26721a72134ac09ab0c2750340eed7
xjstratedgebx <jarret@solvomedia.com>
2014-05-10 16:40:31 -0400

    Adjusting the Halligan PDOStatement_Stub class to allow passing individual rows to the addResult method, and to handle retrieving results just like the QueryResult class expects of the standard PDO class.

--- Tests/PDOStatement.php
+++ Tests/PDOStatement.php
@@ -25,15 +25,15 @@ class PDOStatement_Stub extends \PDOStatement {
 	//---------------------------------------------------------------------------------------------
 	
 
-	public static function addResult($result, $index = NULL)
+	public static function addResult(/* polymorphic */)
 	{
-		if(is_null($index) || !is_numeric($index))
+		if(func_num_args() < 1)
 		{
-			self::$results[] = $result;
+			self::$results[] = array();
 		}
 		else
 		{
-			self::$results[(int) $index] = $result;
+			self::$results[] = func_get_args();
 		}
 	}
 
@@ -43,7 +43,21 @@ class PDOStatement_Stub extends \PDOStatement {
 
 	public function fetch($how = NULL, $orientation = NULL, $offset = NULL)
 	{
-		if(!isset(self::$results[0])) return NULL;
+		switch($how)
+		{
+			case PDO::FETCH_OBJ:
+				if(!isset(self::$results[0])) return array();
+				foreach(self::$results[0] as &$row) { $row = (object) $row; }
+				break;
+
+			case PDO::FETCH_ASSOC:
+				if(!isset(self::$results[0])) return array();
+				array_map(function($row) { return (array) $row; }, self::$results[0]);
+				break;
+		}
+
+		if(!isset(self::$results[0]) || is_null(self::$results[0])) return array();
+		
 		return array_shift(self::$results[0]);
 	}
 
@@ -61,14 +75,14 @@ class PDOStatement_Stub extends \PDOStatement {
 
 		if(count($result))
 		{
-			//Grab he first available row for the result
+			//Grab the first available row for the result
 			$val = array_shift($result);
 
 			//Set the most current result to no longer include the row we just grabbed
 			self::$results[0] = $result;
 
 			//Return the correct column from the row we just grabbed
-			return $val[$column_number];
+			return isset($val[$column_number]) ? $val[$column_number] : NULL;
 		}
 
 		//No values left in the latest result, remove it
